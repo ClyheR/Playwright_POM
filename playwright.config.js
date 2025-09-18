3566f5dbdadd86e1d3cb20d9234f86857c86ef44
ClyheR <121443121+ClyheR@users.noreply.github.com>
2025-09-18 17:52:35 +0530

    Update config for screenshots/videos/traces on failure

--- playwright.config.js
+++ playwright.config.js
@@ -19,11 +19,15 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],                      // shows test progress in console
+    ['allure-playwright'],         // generates allure-results/ folder
+    ['html', { open: 'never' }]    // Playwright’s built-in HTML report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,6 +35,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video:'retain-on-failure'
+
   },
 
   /* Configure projects for major browsers */
